0bfd7af19c913ada0b2793812d9062e68f24a3f1
DanielDev27 <danielbeale27@gmail.com>
2023-03-11 19:46:57 +0200

    Stabilised room spawning

--- Labyrinth/Assets/--PrototypeBuilding/Scripts/RoomGenerator/Room.cs
+++ Labyrinth/Assets/--PrototypeBuilding/Scripts/RoomGenerator/Room.cs
@@ -5,40 +5,44 @@ public class Room : MonoBehaviour {
     [SerializeField] RoomDataObject roomDataObject;
     [SerializeField] GameObject roomGameObject;
     [SerializeField] Vector3 roomPosition;
+    [SerializeField] RoomGenerator roomGenerator;
+    [SerializeField] DoorDirection chosenDoor;
+    [SerializeField] DoorDirection moveDirection;
 
-    public void SetRoomData (RoomDataObject roomDataObject, Vector3 roomPositionInput, out List<RoomDataObject> nextRooms) {
+    public void SetRoomData (RoomDataObject roomDataObject, DoorDirection _chosenDoor, DoorDirection _moveDirection, out List<RoomDataObject> nextRooms) {
         this.roomDataObject = roomDataObject;
-        SetModel (roomPositionInput);
+        chosenDoor = _chosenDoor;
+        moveDirection = _moveDirection;
         SpawnDoors (out nextRooms);
+        SetModel ();
     }
 
-    void SetModel (Vector3 roomPositionInput) {
-        roomGameObject = Instantiate (this.roomDataObject.RoomModel);
+    void SetModel () {
+        roomGameObject = Instantiate (roomDataObject.RoomModel, this.transform);
+        //Debug.Log ($"Fill Empty Room");
+        roomGameObject.transform.localPosition = Vector3.zero;
         roomGameObject.SetActive (true);
-        roomGameObject.transform.parent = this.transform;
-        roomGameObject.transform.position = roomPositionInput;
-        roomPosition = roomPositionInput;
     }
 
     void SpawnDoors (out List<RoomDataObject> nextRooms) {
         nextRooms = new List<RoomDataObject> ();
         if (roomDataObject.doorN) {
-            Debug.Log (message: $"Spawning Door [N]");
+            //Debug.Log (message: $"Spawning Door [N]");
             nextRooms.Add (roomDataObject.GetRandomCompatibleRoom ());
         }
 
         if (roomDataObject.doorE) {
-            Debug.Log (message: $"Spawning Door [E]");
+            //Debug.Log (message: $"Spawning Door [E]");
             nextRooms.Add (roomDataObject.GetRandomCompatibleRoom ());
         }
 
         if (roomDataObject.doorS) {
-            Debug.Log (message: $"Spawning Door [S]");
+            //Debug.Log (message: $"Spawning Door [S]");
             nextRooms.Add (roomDataObject.GetRandomCompatibleRoom ());
         }
 
         if (roomDataObject.doorW) {
-            Debug.Log (message: $"Spawning Door [W]");
+            //Debug.Log (message: $"Spawning Door [W]");
             nextRooms.Add (roomDataObject.GetRandomCompatibleRoom ());
         }
     }
--- Labyrinth/Assets/--PrototypeBuilding/Scripts/RoomGenerator/RoomGenerator.cs
+++ Labyrinth/Assets/--PrototypeBuilding/Scripts/RoomGenerator/RoomGenerator.cs
@@ -1,20 +1,32 @@
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
+
+public enum DoorDirection {
+    North,
+    East,
+    South,
+    West,
+    Non
+}
 
 public class RoomGenerator : MonoBehaviour {
     public static RoomGenerator Instance;
 
     [Header ("Debug Info")]
-    [SerializeField] int i = 0;
+    [SerializeField] Room previousRoom;
 
-    [SerializeField] List<Vector3> roomPosition_Current = new List<Vector3> ();
-
-    [SerializeField] List<Vector3> roomPosition_Next = new List<Vector3> ();
+    [SerializeField] List<DoorDirection> possibleDirections;
+    [SerializeField] DoorDirection chosenDoor;
+    [SerializeField] DoorDirection moveDirection;
 
     [Header ("Input Info")]
     [SerializeField] RoomDataObject startRoom;
+
+    [SerializeField] RoomDataObject nextRoom;
 
     [SerializeField] RoomDataObject[] endRooms;
     [SerializeField] RoomDataObject[] inbetweenRooms;
@@ -28,45 +40,73 @@ public class RoomGenerator : MonoBehaviour {
 
     [ContextMenu ("Generate Rooms")]
     void Generate () {
-        SpawnRoom (startRoom);
+        StartCoroutine (SpawnRoom (startRoom, DoorDirection.North, DoorDirection.Non));
     }
 
-    void SpawnRoom (RoomDataObject roomData) {
-        maxInbetweenRooms -= 1;
-        Room newRoom = Instantiate (roomPrefab);
+    IEnumerator SpawnRoom (RoomDataObject roomData, DoorDirection doorDirection, DoorDirection movedDirection) {
         Vector3 roomPosition;
-        if (maxInbetweenRooms > 0 && i == 0) {
-            roomPosition = new Vector3 (newRoom.transform.position.x + i * 3, 0, newRoom.transform.position.z + i * 3);
-            roomPosition_Current.Add (roomPosition);
-            roomPosition_Next.Add (roomPosition);
-            newRoom.SetRoomData (roomData, roomPosition, out List<RoomDataObject> nextRooms);
-            i++;
-            foreach (var _nextRoom in nextRooms) {
-                SpawnRoom (_nextRoom);
-                //roomGameObject.transform.position = ;
+        Room newRoom = Instantiate (roomPrefab); //make new room
+        //Debug.Log ($"New Empty Room");
+        if (previousRoom) {
+            Vector3 directionOffset = Vector3.zero;
+            switch (doorDirection) {
+                case (DoorDirection.North):
+                    directionOffset = Vector3.forward;
+                    moveDirection = DoorDirection.North;
+                    break;
+                case (DoorDirection.East):
+                    directionOffset = Vector3.right;
+                    moveDirection = DoorDirection.East;
+                    break;
+                case (DoorDirection.South):
+                    directionOffset = Vector3.back;
+                    moveDirection = DoorDirection.South;
+                    break;
+                case (DoorDirection.West):
+                    directionOffset = Vector3.left;
+                    moveDirection = DoorDirection.West;
+                    break;
+                case (DoorDirection.Non):
+                    directionOffset = Vector3.zero;
+                    moveDirection = DoorDirection.Non;
+                    break;
             }
+
+            newRoom.transform.position = previousRoom.transform.position + directionOffset * 3; //move new room based on exit door
+            roomPosition = new Vector3 (previousRoom.transform.position.x, 0, previousRoom.transform.position.z);
         }
 
-        if (maxInbetweenRooms > 0 && i > 0) {
-            roomPosition = new Vector3 (roomPosition_Next[i - 1].x + 3, 0, roomPosition_Next[i - 1].z + 3);
-            roomPosition_Current.Add (roomPosition);
-            roomPosition_Next.Add (roomPosition);
-            newRoom.SetRoomData (roomData, roomPosition, out List<RoomDataObject> nextRooms);
-            i++;
+        if (!previousRoom) {
+            roomPosition = new Vector3 (0, 0, 0);
+        }
+
+        //yield return new WaitForSeconds (5);
+        if (maxInbetweenRooms > 0) {
+            //roomPosition = new Vector3 (previousRoom.transform.position.x, 0, previousRoom.transform.position.z);
+            newRoom.SetRoomData (roomData, chosenDoor, moveDirection, out List<RoomDataObject> nextRooms); //get room data
+            previousRoom = newRoom;
             foreach (var _nextRoom in nextRooms) {
-                SpawnRoom (_nextRoom);
-                //roomGameObject.transform.position = ;
+                possibleDirections = new List<DoorDirection> ();
+                if (_nextRoom.doorN) possibleDirections.Add (DoorDirection.North);
+                if (_nextRoom.doorE) possibleDirections.Add (DoorDirection.East);
+                if (_nextRoom.doorS) possibleDirections.Add (DoorDirection.South);
+                if (_nextRoom.doorW) possibleDirections.Add (DoorDirection.West);
+                chosenDoor = possibleDirections[Random.Range (0, possibleDirections.Count)]; //choose next exit door
+                roomData = _nextRoom;
             }
+
+            maxInbetweenRooms -= 1;
+            possibleDirections.Clear ();
+            yield return new WaitForSeconds (5);
+            StartCoroutine (SpawnRoom (roomData, chosenDoor, moveDirection));
+
+
         }
 
         if (maxInbetweenRooms == 0) {
-            //Room newRoom = Instantiate (roomPrefab);
-            roomPosition = new Vector3 (roomPosition_Next[i - 1].x + 3, 0, roomPosition_Next[i - 1].z + 3);
-            roomPosition_Current.Add (roomPosition);
-            roomPosition_Next.Add (roomPosition);
-            roomData = endRooms[Random.Range (0, endRooms.Length)];
-            newRoom.SetRoomData (roomData, roomPosition, out List<RoomDataObject> nextRooms);
-            i++;
+            //yield return new WaitForSeconds (5);
+            roomData = endRooms[(int) chosenDoor];
+            newRoom.SetRoomData (roomData, chosenDoor, moveDirection, out List<RoomDataObject> nextRooms);
         }
     }
 }
