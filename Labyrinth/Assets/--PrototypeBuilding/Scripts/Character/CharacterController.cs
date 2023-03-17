a5151c05b88e0f58a0f3782bf26015890800c92d
DanielDev27 <danielbeale27@gmail.com>
2023-03-17 18:28:13 +0200

    Start of Character Controller

--- Labyrinth/Assets/--PrototypeBuilding/Scripts/Character/CharacterController.cs
+++ Labyrinth/Assets/--PrototypeBuilding/Scripts/Character/CharacterController.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class CharacterController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CharacterController : MonoBehaviour {
+    Vector2 moveInput;
+    Vector3 moveDirection;
+    Vector2 lookInput;
+    float xRotation;
+    float yRotation;
+    [SerializeField] Rigidbody playerRigidbody;
+    [SerializeField] Transform cameraHolder;
+
+    [Header ("Settings")]
+    [SerializeField] float speedMultiplier = 5;
+
+    [SerializeField] float horizontalSensitivity = 1;
+    [SerializeField] float verticalSensitivity = -1;
+    [SerializeField] float clampMinValue = -90;
+    [SerializeField] float clampMaxValue = 90;
+
+
+    //void Awake () { }
+
+    void FixedUpdate () {
+        OnPlayerMove ();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void LateUpdate () {
+        OnPlayerLook ();
+    }
+
+    public void OnMoveInput (InputAction.CallbackContext incomingValue) {
+        moveInput = incomingValue.ReadValue<Vector2> ();
+    }
+
+    void OnPlayerMove () {
+        moveDirection = moveInput.x * transform.right + moveInput.y * transform.forward;
+
+        Vector3 moveCombined = playerRigidbody.position + new Vector3 (moveDirection.x, 0, moveDirection.z) * (speedMultiplier * Time.fixedDeltaTime);
+        playerRigidbody.MovePosition (moveCombined);
+    }
+
+    public void OnLookInput (InputAction.CallbackContext incomingValue) {
+        lookInput = incomingValue.ReadValue<Vector2> ().normalized;
+    }
+
+    void OnPlayerLook () {
+        xRotation += lookInput.y * verticalSensitivity;
+        yRotation += lookInput.x * horizontalSensitivity;
+        xRotation = Mathf.Clamp (xRotation, clampMinValue, clampMaxValue);
+        transform.rotation = Quaternion.Euler (0, yRotation, 0);
+        cameraHolder.rotation = Quaternion.Euler (xRotation, yRotation, 0);
     }
 }
